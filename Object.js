0462b1dcf047568f3876d6653ee0d7a186aa8a6a
Neil Munro <neilmunro@gmail.com>
2016-04-04 10:08:44 +0100

    Beefed up isObject and isEmpty.
    Beefed up isObject and isEmpty.

--- Object.js
+++ Object.js
@@ -1,14 +1,14 @@
 if(Object.prototype.isEmpty === undefined) {
   Object.prototype.isEmpty = function(o) {
-    return Object.keys(o).reduce((p, n) => n ? p+1 : p, 0) <= 0;
+		if(!Object.isObject(o)) throw "TypeError: Argument is not an object.";
+		return(Object.keys(o).length === 0); 
   };
 }
 
 if(Object.prototype.isObject === undefined) {
   Object.prototype.isObject = function(o) {
     if(typeof(o) !== "object") return false;
-    else if(Array.isArray(o) || o === null) return false;
-    else return true;
+		return(Array.isArray(o) || o === null) ? false : true;
   };
 }
 
@@ -17,18 +17,18 @@ if(Object.prototype.isEqual === undefined) {
     if(!Object.isObject(o1) || !Object.isObject(o2)) return false;
     const keysO1 = Object.keys(o1);
     const keysO2 = Object.keys(o2);
-    
+
     // Ensure they're in the same order.
     keysO1.sort();
     keysO2.sort();
-    
+
     // They're both empty.
     if(Object.isEmpty(o1) && Object.isEmpty(o2)) return true;
-    
+
     // Have different sets of keys.
     if(keysO1.toString() !== keysO2.toString()) return false;
-    
+
     // Values within the keys are different.
     return keysO1.every((elm, count, arr) => o1[elm] === o2[elm]);
-  }; 
+  };
 }
